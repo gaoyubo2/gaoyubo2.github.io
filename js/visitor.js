08e849c3d01750e7cb4b5e2439b1bd1fdc6659c6
gaoyubo2 <673354892@qq.com>
2024-01-11 01:22:02 +0800

    Site updated: 2024-01-11 01:22:01

--- js/visitor.js
+++ js/visitor.js
@@ -1,11 +1,37 @@
-fetch('https://api.gmit.vip/Api/UserInfo').then(data=>data.json()).then(data=>{
-    let html = '<style>.visitor_location{color:#cb4c46;font-weight:bold;}.visitor_ip{color:#2d80c2;font-weight:bold;}</style>'
+//获取当前IP地址和浏览器标识
+function getBrowserInfo() {
+    var agent = navigator.userAgent.toLowerCase();
+
+    var regStr_ie = /msie [\d.]+;/gi;
+    var regStr_ff = /firefox\/[\d.]+/gi
+    var regStr_chrome = /chrome\/[\d.]+/gi;
+    var regStr_saf = /safari\/[\d.]+/gi;
+
+    //IE
+    if (agent.indexOf("msie") > 0) {
+        return agent.match(regStr_ie);
+    }
+
+    //firefox
+    if (agent.indexOf("firefox") > 0) {
+        return agent.match(regStr_ff);
+    }
+
+    //Chrome
+    if (agent.indexOf("chrome") > 0) {
+        return agent.match(regStr_chrome);
+    }
+
+    //Safari
+    if (agent.indexOf("safari") > 0 && agent.indexOf("chrome") < 0) {
+        return agent.match(regStr_saf);
+    }
+}
+
+let html = '<style>.visitor_location{color:#cb4c46;font-weight:bold;}.visitor_ip{color:#2d80c2;font-weight:bold;}</style>'
     html += '<div class="visitor">'
-    html += '欢迎来自 ' + '<span class="visitor_location">' + data.data.location + '</span>' + ' 的小伙伴！'
+    html += '欢迎来自 ' + '<span class="visitor_location">' + returnCitySN["cname"] + '</span>' + ' 的小伙伴！'
     html += '</br>'
-    html += '访问IP：' + '<span class="visitor_ip">' + data.data.ip + '</span>'
+    html += '访问IP：' + '<span class="visitor_ip">' + returnCitySN["cip"] + '</span>'
     html += '</div>'
     document.getElementById('visitor-container').innerHTML = html
-}).catch(function(error) {
-    console.log(error);
-});
